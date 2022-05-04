6e648556763249771b249641ccb01a43aa7f6e5d
lenormandSeb <lenormand.sbn@gmail.com>
2022-05-04 17:39:15 +0000

    feat/calcul_vitesse

--- hello.f90
+++ hello.f90
@@ -1,13 +1,42 @@
-program hello
-    ! implicit none
+program speed_calculator
+    implicit none
 
-    integer(2) :: number
+    integer :: n
 
-    print *, 'Type a number :'
-    read(*,*) number
+    TYPE calcul
+    real :: speed
+    real :: distance
+    real :: time_do
+    integer, dimension(3) :: convert
+    END TYPE calcul
 
-    do n = 1, 10
-        n_fact = n_fact*number
-        print *, number, " ", n_fact
+    Type(calcul) :: calculator
+
+    print *, 'Quel est la vitesse ? (en km/h)'
+    read(*,*) calculator%speed
+
+    print *, 'Quel est la distance ? (en kilometres)'
+    read(*,*) calculator%distance
+
+    calculator%time_do = calculator%distance / calculator%speed
+
+    do n = 1,size(calculator%convert)
+        print *, n
+        calculator%convert(n) = int(calculator%time_do)
+        calculator%time_do = (calculator%time_do - calculator%convert(n)) * 60
     end do
-end program hello
+
+    print *, calculator%convert(1), ' Heures'
+    print *, calculator%convert(2), ' minutes'
+    print *, calculator%convert(3), ' secondes'
+
+    call print_un_coucou
+
+end program speed_calculator
+
+
+subroutine print_un_coucou
+    character(len=80) :: jesuisunprint = 'coucou'
+
+    print *, jesuisunprint
+end subroutine
